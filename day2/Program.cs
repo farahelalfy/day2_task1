68bc7fba185ca8a648afce44b2cc46e6ec97fd3b
farah <farah@LAPTOP-RJCB7SM0>
2024-07-11 14:08:06 +0300

    Add project files.

---
+++ day2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace day2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("plz enter a num");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"the multplication for {num}");
+            for (int i = 1; i < 12; i++)
+            {
+                Console.WriteLine($" {num}*{i}={i * num}");
+            }
+        }
+    }
+}
